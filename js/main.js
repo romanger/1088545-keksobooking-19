f7cdeb4b15939a5575f1c33e9479e5b679863aed
Roman Gertsvolf <roman.gertsvolf@gmail.com>
2020-01-29 13:56:05 +0200

    add pins to map

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var MAP_WIDTH = 1200;
 var LOCATION_MAX = 630;
 var LOCATION_MIN = 130;
 var ROOMS_MAX = 4;
@@ -5,10 +6,21 @@ var ROOMS_MIN = 1;
 var GUESTS_MAX = 4;
 var GUESTS_MIN = 1;
 
+var PIN_PARAMS = {
+  "width": 50,
+  "height": 70
+}
+
 var OFFER_TITLES = [
   "Лучшая Квартира В Токио Только летом",
   "Квартира на съем только на месяц в самом центре",
   "Прекрасный вид с 13 го этажа"
+];
+
+var OFFER_DESCRIPTIONS = [
+  "Офигенная квартира",
+  "Обалденная квартира",
+  "Если будет еще хоть чуточку лучше я сойду с ума"
 ];
 
 var CHECKIN_CHECKOUT_TIMES = ["12:00", "13:00", "14:00"];
@@ -46,14 +58,47 @@ var createDataArray = function () {
         "guests": getRandomeInRange(GUESTS_MIN, GUESTS_MAX),
         "checkin": getRandomElement(CHECKIN_CHECKOUT_TIMES),
         "checkout": getRandomElement(CHECKIN_CHECKOUT_TIMES),
-        "features": "",
-        "description": "",
+        "features": FEATURES,
+        "description": getRandomElement(OFFER_DESCRIPTIONS),
         "photos": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
       },
       "location": {
-        "x": 0,
+        "x": getRandomeInRange(0, MAP_WIDTH),
         "y": getRandomeInRange(LOCATION_MIN, LOCATION_MAX)
       }
     }
   }
+  return adsArray;
 }
+
+var addPin = function (obj, node) {
+  var img = node.querySelector('img');
+  node.style.left = (obj.location.x - PIN_PARAMS.width / 2) + "px";
+  node.style.top = (obj.location.y - PIN_PARAMS.height) + "px";
+  img.src = obj.author.avatar;
+  img.alt = obj.offer.title;
+  return node;
+}
+
+var fillTheMap = function (arr, templet) {
+  for(var i = 0; i < arr.length; i++) {
+    var pin = templet.cloneNode(true);
+    pin = addPin(arr[i], pin);
+    fragment.appendChild(pin);
+  }
+  return fragment;
+}
+
+var offers = createDataArray();
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+
+var mapPinsArea = map.querySelector('.map__pins');
+
+var fragment = document.createDocumentFragment();
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+fragment = fillTheMap(offers,pinTemplate);
+
+mapPinsArea.appendChild(fragment);
