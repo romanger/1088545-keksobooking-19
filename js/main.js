234f50dc0417e37a1afd962b6f9c35f1359ff603
Roman Gertsvolf <roman@12pixels.co>
2020-02-02 18:32:36 +0200

    fix02

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var MAP_WIDTH = 1200;
 var LOCATION_MAX = 630;
 var LOCATION_MIN = 130;
+var MIN_PRICE = 300;
+var MAX_PRICE = 1000;
 var ROOMS_MAX = 4;
 var ROOMS_MIN = 1;
 var GUESTS_MAX = 4;
@@ -26,9 +27,20 @@ var OFFER_DESCRIPTIONS = [
 ];
 
 var CHECKIN_CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
-var OFFER_PRISES = [500, 400, 399, 800, 1000, 200, 100];
 var OFFERS_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+var PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+
+var map = document.querySelector('.map');
+var mapWidth = map.offsetWidth;
+var mapPinsArea = map.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
 
 var getRandomeInRange = function (min, max) {
   var num = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -39,6 +51,16 @@ var getRandomElement = function (thisArray) {
   var randomElement = thisArray[Math.floor(Math.random() * thisArray.length)];
   return randomElement;
 };
+
+var getRandomArray = function (thisArray) {
+  var randomLength = Math.floor(Math.random() * ((thisArray.length) - 1 + 1)) + 1;
+  var randomArray = [];
+
+  for (var i = 0; i < randomLength; i++) {
+    randomArray[i] = thisArray[i];
+  }
+  return randomArray;
+}
 
 var createDataArray = function () {
   var objectsNumber = 8;
@@ -54,18 +76,18 @@ var createDataArray = function () {
       'offer': {
         'title': getRandomElement(OFFER_TITLES),
         'address': locationX.toString() + ',' + locationY.toString(),
-        'price': getRandomElement(OFFER_PRISES),
+        'price': getRandomeInRange(MIN_PRICE, MAX_PRICE),
         'type': getRandomElement(OFFERS_TYPES),
         'rooms': getRandomeInRange(ROOMS_MIN, ROOMS_MAX),
         'guests': getRandomeInRange(GUESTS_MIN, GUESTS_MAX),
         'checkin': getRandomElement(CHECKIN_CHECKOUT_TIMES),
         'checkout': getRandomElement(CHECKIN_CHECKOUT_TIMES),
-        'features': FEATURES,
+        'features': getRandomArray(FEATURES),
         'description': getRandomElement(OFFER_DESCRIPTIONS),
-        'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+        'photos': getRandomArray(PHOTOS),
       },
       'location': {
-        'x': getRandomeInRange(0, MAP_WIDTH),
+        'x': getRandomeInRange(0, mapWidth),
         'y': getRandomeInRange(LOCATION_MIN, LOCATION_MAX)
       }
     };
@@ -75,32 +97,26 @@ var createDataArray = function () {
 
 var addPin = function (obj, node) {
   var img = node.querySelector('img');
-  node.style.left = (obj.location.x - PIN_PARAMS.width / 2) + 'px';
-  node.style.top = (obj.location.y - PIN_PARAMS.height) + 'px';
   img.src = obj.author.avatar;
   img.alt = obj.offer.title;
+
+  node.style.left = (obj.location.x - PIN_PARAMS.width / 2) + 'px';
+  node.style.top = (obj.location.y - PIN_PARAMS.height) + 'px';
+
   return node;
 };
 
-var fillTheMap = function (arr, templet) {
+var fillTheMap = function (arr, template, destination) {
+  var fragment = document.createDocumentFragment();
   for (var i = 0; i < arr.length; i++) {
-    var pin = templet.cloneNode(true);
+    var pin = template.cloneNode(true);
     pin = addPin(arr[i], pin);
     fragment.appendChild(pin);
   }
-  return fragment;
+  destination.appendChild(fragment);
 };
 
 var offers = createDataArray();
-
-var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
-var mapPinsArea = map.querySelector('.map__pins');
-
-var fragment = document.createDocumentFragment();
-var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-
-fragment = fillTheMap(offers, pinTemplate);
-
-mapPinsArea.appendChild(fragment);
+fillTheMap(offers, pinTemplate, mapPinsArea);
