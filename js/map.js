c51fe056d83f028d6aa8ea32dd98d61c4a9ae517
Roman Gertsvolf <roman.gertsvolf@gmail.com>
2020-02-16 13:28:17 +0200

    module6-task2

--- js/map.js
+++ js/map.js
@@ -1,19 +1,23 @@
 'use strict';
 
 (function () {
+  var map = document.querySelector('.map');
+  var mapPinsArea = map.querySelector('.map__pins');
+  var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-  var fillTheMap = function (arr, template, destination) {
+  var onActivation = function (arr) {
     var fragment = document.createDocumentFragment();
+    window.tools.removeError('.error-element');
     for (var i = 0; i < arr.length; i++) {
-      var pin = template.cloneNode(true);
+      var pin = pinTemplate.cloneNode(true);
       pin = window.pin.addPin(arr[i], pin);
       fragment.appendChild(pin);
     }
-    destination.appendChild(fragment);
+    mapPinsArea.appendChild(fragment);
   };
 
   window.map = {
-    fillTheMap: fillTheMap
+    onActivation: onActivation,
   };
 
 })();
